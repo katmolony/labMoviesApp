25d54b67a86b6a47a0f954e9801fbc0f42c0d9b2
Kate Molony <katemolony96@gmail.com>
2024-06-17 13:20:40 +0100

    Added routing and live data from API.

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
 import Grid from "@mui/material/Grid";
 import MovieList from "../components/movieList";
-import { BaseMovieListProps } from "../types/interfaces";
- 
+import { BaseMovieListProps, BaseMovieProps } from "../types/interfaces";
+
 const styles = {
   root: {
     padding: "20px",
   },
 };
 
+const MovieListPage: React.FC= () => {
+  const [movies, setMovies] = useState<BaseMovieProps[]>([]);
 
-
-const MovieListPage: React.FC<BaseMovieListProps> = ({movies}) => {
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json)
+        return json.results;
+      })
+      .then((movies) => {
+        setMovies(movies);
+      });
+  }, []);
   return (
     <Grid container sx={styles.root}>
       <Grid item xs={12}>
         <Header title={"Home Page"} />
       </Grid>
       <Grid item container spacing={5}>
-        <MovieList movies={movies}></MovieList>
+      <MovieList movies={movies}></MovieList>  //change to use movies from state instead of props.movies
       </Grid>
     </Grid>
   );
