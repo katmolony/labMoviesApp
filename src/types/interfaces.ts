affd067731b0fb58b262f0e1f288e005f5ecd291
Kate Molony <katemolony96@gmail.com>
2024-06-19 17:29:20 +0100

    Show movie review excerpts; Refactor Details page.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -54,3 +54,9 @@ export interface MoviePageProps {
 export interface MovieListPageTemplateProps extends BaseMovieListProps {
   title: string;
 }
+
+export interface Review{
+  id: string;
+  content: string
+  author: string
+}
