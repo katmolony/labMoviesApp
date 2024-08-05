d0f1a9cea0150e2f25ce314a3c72f2955d696178
Kate Molony <katemolony96@gmail.com>
2024-08-05 11:53:27 +0100

    Add new review support.

--- src/contexts/moviesContext.tsx
+++ src/contexts/moviesContext.tsx
@@ -1,47 +1,62 @@
 import React, { useState, useCallback } from "react";
-import { BaseMovieProps } from "../types/interfaces";
-
+import { BaseMovieProps, Review } from "../types/interfaces";
 
 interface MovieContextInterface {
-    favourites: number[];
-    addToFavourites: ((movie: BaseMovieProps) => void);
-    removeFromFavourites: ((movie: BaseMovieProps) => void);
+  favourites: number[];
+  addToFavourites: (movie: BaseMovieProps) => void;
+  removeFromFavourites: (movie: BaseMovieProps) => void;
+  addReview: (movie: BaseMovieProps, review: Review) => void; // NEW
 }
 const initialContextState: MovieContextInterface = {
-    favourites: [],
-    addToFavourites: () => {},
-    removeFromFavourites: () => {}
+  favourites: [],
+  addToFavourites: () => {},
+  removeFromFavourites: () => {},
+  addReview: (movie, review) => {
+    movie.id, review;
+  }, // NEW
 };
 
-export const MoviesContext = React.createContext<MovieContextInterface>(initialContextState);
+export const MoviesContext =
+  React.createContext<MovieContextInterface>(initialContextState);
 
-const MoviesContextProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
-    const [favourites, setFavourites] = useState<number[]>([]);
+const MoviesContextProvider: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  const [myReviews, setMyReviews] = useState<Review[]>([]); // NEW
+  const [favourites, setFavourites] = useState<number[]>([]);
 
-    const addToFavourites = useCallback((movie: BaseMovieProps) => {
-        setFavourites((prevFavourites) => {
-            if (!prevFavourites.includes(movie.id)) {
-                return [...prevFavourites, movie.id];
-            }
-            return prevFavourites;
-        });
-    }, []);
+  const addToFavourites = useCallback((movie: BaseMovieProps) => {
+    setFavourites((prevFavourites) => {
+      if (!prevFavourites.includes(movie.id)) {
+        return [...prevFavourites, movie.id];
+      }
+      return prevFavourites;
+    });
+  }, []);
 
-    const removeFromFavourites = useCallback((movie: BaseMovieProps) => {
-        setFavourites((prevFavourites) => prevFavourites.filter((mId) => mId !== movie.id));
-    }, []);
-
-    return (
-        <MoviesContext.Provider
-            value={{
-                favourites,
-                addToFavourites,
-                removeFromFavourites,
-            }}
-        >
-            {children}
-        </MoviesContext.Provider>
+  const removeFromFavourites = useCallback((movie: BaseMovieProps) => {
+    setFavourites((prevFavourites) =>
+      prevFavourites.filter((mId) => mId !== movie.id)
     );
+  }, []);
+
+  const addReview = (movie: BaseMovieProps, review: Review) => {
+    // NEW
+    setMyReviews({ ...myReviews, [movie.id]: review });
+  };
+
+  return (
+    <MoviesContext.Provider
+      value={{
+        favourites,
+        addToFavourites,
+        removeFromFavourites,
+        addReview, // NEW
+      }}
+    >
+      {children}
+    </MoviesContext.Provider>
+  );
 };
 
 export default MoviesContextProvider;
--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from "./pages/addMovieReviewPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -28,6 +29,7 @@ const App = () => {
         <SiteHeader /> {/* New Header  */}
         <MoviesContextProvider>
           <Routes>
+            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
             <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
             <Route path="/reviews/:id" element={<MovieReviewPage />} />
             <Route
